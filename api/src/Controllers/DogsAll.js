3002f8eb17da3c15aa64f75933c884a818885e92
andresjf93 <129121808+andresjf93@users.noreply.github.com>
2023-09-08 18:25:27 -0500

    modificado

--- api/src/Controllers/DogsAll.js
+++ api/src/Controllers/DogsAll.js
@@ -1,11 +1,12 @@
 require("dotenv").config();
 const axios = require("axios");
+const { API_KEY } = process.env;
 
 const URL = `https://api.thedogapi.com/v1/breeds`;
 
 const getdogsAll = async (req, res) => {
   try {
-    const { data } = await axios.get(`${URL}`);
+    const { data } = await axios.get(`${URL}?api_key=${API_KEY}`);
     const dogsAllApi = [];
     data.forEach((dogA) => {
       const dogApi = {
