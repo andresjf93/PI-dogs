c3fcce371309206fbed6b15db24d93d6f6b50ac3
andresjf93 <129121808+andresjf93@users.noreply.github.com>
2023-09-11 02:22:49 -0500

    test deploy

--- client/src/Components/Card/DogCard.jsx
+++ client/src/Components/Card/DogCard.jsx
@@ -6,6 +6,7 @@ import { Link } from "react-router-dom";
 function DogCard({ dog }) {
   const { id, name, image, temperament, weight } = dog;
 
+  
   return (
     
     <Atropos  className="card">
