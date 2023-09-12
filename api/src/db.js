fbfc0a0226f432cee2481f071b1a9f952172b550
andresjf93 <129121808+andresjf93@users.noreply.github.com>
2023-09-12 12:59:22 -0500

    vscl

--- api/src/db.js
+++ api/src/db.js
@@ -2,11 +2,11 @@ require("dotenv").config();
 const { Sequelize } = require("sequelize");
 const fs = require("fs");
 const path = require("path");
-const { DB_URL } = process.env;
+const { DB_URL_URL } = process.env;
 
 
 const sequelize = new Sequelize(
-  `${DB_URL}`,
+  `${DB_URL_URL}`,
   {
     logging: false, // set to console.log to see the raw SQL queries
     native: false, // lets Sequelize know we can use pg-native for ~30% more speed
